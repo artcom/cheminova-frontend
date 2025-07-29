0916bdb4e5f19e1328f9ba47d420cda449e3ac50
Dino Lindlau <dino.lindlau@artcom.de>
2025-07-29 19:02:37 +0200

    Create Headline UI component

---
+++ src/components/UI/Headline.jsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components"
+
+const Headline = styled.div`
+  color: #fff;
+  text-align: center;
+
+  font-family: "Bricolage Grotesque";
+  font-size: 2.625rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+
+export default Headline
