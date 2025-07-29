00263b927e1438dbe48e58aadf693e4d49a181f8
Dino Lindlau <dino.lindlau@artcom.de>
2025-07-29 19:03:41 +0200

    Create SubHeadline component

---
+++ src/components/UI/SubHeadline.jsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components"
+
+const SubHeadline = styled.div`
+  color: #fff;
+  text-align: center;
+
+  font-family: "Bricolage Grotesque";
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+
+export default SubHeadline
