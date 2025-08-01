736f8dcbf1408763217661d3958cd9303bdf88eb
Dino Lindlau <dino.lindlau@artcom.de>
2025-08-01 12:08:34 +0200

    Remove unused code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,5 @@
-import useGlobalState from "@hooks/useGlobalState"
 import DemoPage from "@components/DemoPage"
 
 export default function App() {
-  const { selectedCharacter, setSelectedCharacter } = useGlobalState()
-
-  return (
-    <>
-      <h1> Cheminova Frontend</h1>
-      <p> Selected Character: {selectedCharacter ?? "none Selected"}</p>
-      <button
-        onClick={() =>
-          setSelectedCharacter(selectedCharacter === "bob" ? "alice" : "bob")
-        }
-      >
-        Set selected character to{" "}
-        {selectedCharacter === "bob" ? "alice" : "bob"}
-      </button>
-      <DemoPage />
-    </>
-  )
+  return <DemoPage />
 }
