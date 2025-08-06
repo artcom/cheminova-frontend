2263ca1c449c0c9b4bb920641025139805616d3d
till gilsbach <till.gilsbach@artcom.de>
2025-08-06 17:46:42 +0200

    Refactor

--- src/components/Gallery/components/SpecialTile.jsx
+++ src/components/Gallery/components/SpecialTile.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unknown-property */
-export default function SpecialTile({ position = [1, 1, 1], size = [1, 1] }) {
+export default function SpecialTile({ position = [1, 1, 1], scale = [1, 1] }) {
   return (
     <mesh position={position}>
-      <planeGeometry args={size} /> {/* Width: 2, Height: 0.5 */}
+      <planeGeometry args={scale} />
       <meshBasicMaterial color="red" />
     </mesh>
   )
