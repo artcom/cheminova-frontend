90da26eb8e27108a7a4a36ec5bc179f75ca6223e
Dino Lindlau <dino.lindlau@artcom.de>
2025-08-07 11:06:45 +0200

    Update eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,14 @@ import ReactThree from "@react-three/eslint-plugin"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
   {
     files: ["**/*.config.{js,mjs,cjs}", "vite.config.js", "eslint.config.js"],
     languageOptions: {
@@ -22,26 +29,46 @@ export default [
       },
     },
   },
-  ...pluginQuery.configs["flat/recommended"],
+
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  eslintConfigPrettier,
+
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+      "react-compiler": reactCompiler,
+      "@tanstack/query": pluginQuery,
+    },
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...pluginReact.configs.flat["jsx-runtime"].rules,
+      ...pluginQuery.configs["flat/recommended"].rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
+      "react/prop-types": "off",
+      "react-hooks/exhaustive-deps": "warn",
+    },
     settings: {
       react: {
         version: "detect",
       },
     },
+  },
+
+  {
+    files: ["src/components/Gallery/**/*.{js,jsx}"],
     plugins: {
-      "react-hooks": pluginReactHooks,
-      "react-compiler": reactCompiler,
       "@react-three": ReactThree,
     },
     rules: {
-      "react-compiler/react-compiler": "error",
-      "react/prop-types": 0,
+      ...ReactThree.configs.recommended.rules,
+      "react/no-unknown-property": "off",
     },
   },
+
+  eslintConfigPrettier,
 ]
--- src/components/Gallery/components/SpecialTile.jsx
+++ src/components/Gallery/components/SpecialTile.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unknown-property */
 export default function SpecialTile({ position = [1, 1, 1], scale = [1, 1] }) {
   return (
     <mesh position={position}>
