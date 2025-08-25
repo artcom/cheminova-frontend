bbfbbd5344f334ea5482d2c4694d969932f49d4c
Dino Lindlau <dino.lindlau@artcom.de>
2025-08-25 14:50:34 +0200

    Refactor carousel to extract character card component

---
+++ src/components/CharacterShowcase/components/CharacterCard.jsx
@@ -0,0 +1,56 @@
+import { motion } from "motion/react"
+import { styled } from "styled-components"
+import { CAROUSEL_ANIMATION } from "../utils/transformUtils"
+
+const CharacterCardContainer = styled(motion.div)`
+  flex: 0 0 100dvw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  user-select: none;
+`
+
+const CharacterImage = styled(motion.img)`
+  width: auto;
+  height: 60dvh;
+  margin-bottom: 5.25rem;
+  object-fit: contain;
+  filter: ${(props) =>
+    `drop-shadow(0 ${0.5 + props.$shadowIntensity * 0.25}rem ${0.75 + props.$shadowIntensity * 0.5}rem rgba(0, 0, 0, ${0.4 - props.$shadowIntensity * 0.1}))`};
+`
+
+const CharacterCard = ({ character, scale, shadowIntensity }) => {
+  return (
+    <CharacterCardContainer
+      initial={{ opacity: 0, y: 20 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        scale: scale,
+      }}
+      transition={{
+        duration: 0.5,
+        delay: 0.4,
+        ease: "easeOut",
+        scale: CAROUSEL_ANIMATION,
+      }}
+    >
+      <CharacterImage
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.6,
+          delay: 0.6,
+          ease: "easeOut",
+        }}
+        src={character.image}
+        alt={character.name}
+        $shadowIntensity={shadowIntensity}
+      />
+    </CharacterCardContainer>
+  )
+}
+
+export default CharacterCard
--- src/components/CharacterShowcase/components/CharacterCarousel.jsx
+++ src/components/CharacterShowcase/components/CharacterCarousel.jsx
@@ -2,7 +2,8 @@ import { motion } from "motion/react"
 import { styled } from "styled-components"
 import { CHARACTER_DATA } from "../constants"
 import { useCharacterCarousel } from "../../../hooks/useCharacterCarousel"
-import { CAROUSEL_ANIMATION, DRAG_CONFIG } from "../utils/transformUtils"
+import { DRAG_CONFIG } from "../utils/transformUtils"
+import CharacterCard from "./CharacterCard"
 
 const CarouselContainer = styled(motion.div)`
   width: 100dvw;
@@ -19,25 +20,6 @@ const DraggableWrapper = styled(motion.div)`
   display: flex;
   height: 100%;
   width: ${(props) => `${props.$charactersLength * 100}dvw`};
-`
-
-const CharacterCardContainer = styled(motion.div)`
-  flex: 0 0 100dvw;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  user-select: none;
-`
-
-const CharacterImage = styled(motion.img)`
-  width: auto;
-  height: 60dvh;
-  margin-bottom: 5.25rem;
-  object-fit: contain;
-  filter: ${(props) =>
-    `drop-shadow(0 ${0.5 + props.$shadowIntensity * 0.25}rem ${0.75 + props.$shadowIntensity * 0.5}rem rgba(0, 0, 0, ${0.4 - props.$shadowIntensity * 0.1}))`};
 `
 
 export default function CharacterCarousel({
@@ -84,37 +66,5 @@ export default function CharacterCarousel({
         })}
       </DraggableWrapper>
     </CarouselContainer>
-  )
-}
-
-const CharacterCard = ({ character, scale, shadowIntensity }) => {
-  return (
-    <CharacterCardContainer
-      initial={{ opacity: 0, y: 20 }}
-      animate={{
-        opacity: 1,
-        y: 0,
-        scale: scale,
-      }}
-      transition={{
-        duration: 0.5,
-        delay: 0.4,
-        ease: "easeOut",
-        scale: CAROUSEL_ANIMATION,
-      }}
-    >
-      <CharacterImage
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.6,
-          delay: 0.6,
-          ease: "easeOut",
-        }}
-        src={character.image}
-        alt={character.name}
-        $shadowIntensity={shadowIntensity}
-      />
-    </CharacterCardContainer>
   )
 }
