1346b9812e1d6a97310e0890798dbf14f79181e9
Dino Lindlau <dino.lindlau@artcom.de>
2025-08-26 11:51:38 +0200

    Refactor device hook

--- src/hooks/useDevicePlatform.js
+++ src/hooks/useDevicePlatform.js
@@ -8,25 +8,24 @@ export default function useDevicePlatform() {
   })
 
   useEffect(() => {
-    const userAgent = navigator.userAgent || ""
+    const userAgent = navigator.userAgent
 
     const isIOS =
       /iPhone|iPod/.test(userAgent) ||
       (/iPad/.test(userAgent) && !window.MSStream)
-
     const isAndroid = /android/i.test(userAgent)
-    let os = "unknown"
-    if (isIOS) {
-      os = "iOS"
-    } else if (isAndroid) {
-      os = "Android"
-    } else if (/Windows/.test(userAgent)) {
-      os = "Windows"
-    } else if (/Macintosh|Mac OS X/.test(userAgent)) {
-      os = "MacOS"
-    } else if (/Linux/.test(userAgent)) {
-      os = "Linux"
-    }
+
+    const os = isIOS
+      ? "iOS"
+      : isAndroid
+        ? "Android"
+        : /Windows/.test(userAgent)
+          ? "Windows"
+          : /Macintosh|Mac OS X/.test(userAgent)
+            ? "MacOS"
+            : /Linux/.test(userAgent)
+              ? "Linux"
+              : "unknown"
 
     setPlatform({ os, isIOS, isAndroid })
   }, [])
