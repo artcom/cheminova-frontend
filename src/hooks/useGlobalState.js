37496ad43b9dd1a7a7506f09f8c1d2af0411c6fc
Dino Lindlau <dino.lindlau@artcom.de>
2025-08-26 11:50:51 +0200

    Refactor GlobalState

--- src/hooks/useGlobalState.js
+++ src/hooks/useGlobalState.js
@@ -2,9 +2,5 @@ import { StateContext } from "@/GlobalState"
 import { useContext } from "react"
 
 export default function useGlobalState() {
-  const context = useContext(StateContext)
-  if (context === undefined) {
-    throw new Error("useGlobalState must be used within a StateProvider")
-  }
-  return context
+  return useContext(StateContext)
 }
