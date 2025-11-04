3fb7a20cf75e880e17a9f8781a69077bfa573a34
till gilsbach <till.gilsbach@artcom.de>
2025-11-04 15:30:31 +0100

    Add basic transition

--- src/components/PhotoCapture/PhotoCapture.jsx
+++ src/components/PhotoCapture/PhotoCapture.jsx
@@ -139,6 +139,7 @@ export default function PhotoCapture({
                     transform: isActive ? "translate(-9.7rem, -14rem)" : "none", // Adjust for centering
                     zIndex: isActive ? 2 : 1,
                     opacity: isActive ? 1 : 0.5,
+                    transition: "all 0.3s ease-in-out",
                   }}
                 >
                   {!taskImages[index] && (
