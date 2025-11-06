e4e0791278e118e5cc2cd6c7a0e6fa9813ea35b2
till gilsbach <till.gilsbach@artcom.de>
2025-11-06 12:40:43 +0100

    Improve swipe functionality

--- src/hooks/useSwipe.js
+++ src/hooks/useSwipe.js
@@ -1,11 +1,18 @@
 import { useRef } from "react"
 
-export const useSwipe = (onSwipeLeft, onSwipeRight, swipeThreshold = 50) => {
+export const useSwipe = (
+  onSwipeLeft,
+  onSwipeRight,
+  swipeThreshold = 50,
+  timeThreshold = 100,
+) => {
   const touchStartX = useRef(0)
   const touchEndX = useRef(0)
+  const touchStartTime = useRef(0)
 
   const handleTouchStart = (event) => {
     touchStartX.current = event.touches[0].clientX
+    touchStartTime.current = Date.now()
   }
 
   const handleTouchMove = (event) => {
@@ -14,6 +21,12 @@ export const useSwipe = (onSwipeLeft, onSwipeRight, swipeThreshold = 50) => {
 
   const handleTouchEnd = () => {
     const swipeDistance = touchStartX.current - touchEndX.current
+    const touchDuration = Date.now() - touchStartTime.current
+
+    // Check if the touch duration is too long to be a swipe
+    if (touchDuration < timeThreshold) {
+      return
+    }
 
     if (swipeDistance > swipeThreshold) {
       onSwipeLeft && onSwipeLeft()
