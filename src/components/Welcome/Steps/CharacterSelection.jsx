a88485ecc39d002b33220881c0829f173c2c4567
Dino Lindlau <dino.lindlau@artcom.de>
2025-11-24 19:56:07 +0100

    Preload images depending on character

--- src/components/Welcome/Steps/CharacterSelection.jsx
+++ src/components/Welcome/Steps/CharacterSelection.jsx
@@ -1,5 +1,6 @@
 import { getCharacterSlug } from "@/utils/characterSlug"
-import { useState } from "react"
+import { preloadImages } from "@/utils/preloadImages"
+import { useEffect, useState } from "react"
 import { useLocation, useNavigate, useOutletContext } from "react-router-dom"
 
 import CharacterCarousel from "../components/CharacterCarousel"
@@ -18,6 +19,35 @@ export default function CharacterSelection() {
   const currentCharacterSlug = currentCharacter
     ? getCharacterSlug(currentCharacter, characters)
     : null
+
+  useEffect(() => {
+    if (!currentCharacter) return
+
+    const preloadAssets = async () => {
+      // The introduction is the first child of the character node
+      const introduction = currentCharacter.children?.[0]
+
+      if (!introduction) return
+
+      const imagesToPreload = [
+        introduction.backgroundImage?.file,
+        introduction.image?.file,
+        introduction.characterImage?.file,
+        currentCharacter.selectedImage,
+        currentCharacter.characterImage?.file,
+      ].filter(Boolean)
+
+      // Start preloading images
+      preloadImages(imagesToPreload)
+
+      // Preload Rive animation for artist
+      if (currentCharacterSlug === "artist") {
+        fetch("/amaraWriting.riv").then((response) => response.blob())
+      }
+    }
+
+    preloadAssets()
+  }, [selectedIndex, currentCharacter, currentCharacterSlug])
 
   const handleNext = () => {
     if (selectedIndex < characters.length - 1) {
