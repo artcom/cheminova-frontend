575dd6d9af0de8805bd20a0a334835cdab472f74
Dino Lindlau <dino.lindlau@artcom.de>
2026-01-28 11:55:34 +0100

    For dev use root path config.json

--- src/entry.client.jsx
+++ src/entry.client.jsx
@@ -2,8 +2,9 @@ import { setConfig } from "@/api/config"
 
 async function loadConfig() {
   try {
-    const response = await fetch("./config.json")
-    console.info("fetching from ./config.json")
+    const configUrl = import.meta.env.DEV ? "/config.json" : "./config.json"
+    const response = await fetch(configUrl)
+    console.info(`fetching from ${configUrl}`)
     const config = await response.json()
     console.info("config loaded", config)
     setConfig(config)
